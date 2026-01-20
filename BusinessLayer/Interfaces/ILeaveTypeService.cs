e0e753396e27395581c44a4988d942f294f134db
KishoreSetti <kishore@cortracker360.com>
2026-01-20 16:13:29 +0530

    Leave_Type_api
    Leave_Type_api

---
+++ BusinessLayer/Interfaces/ILeaveTypeService.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface ILeaveTypeService
+    {
+        Task<List<LeaveTypeDto>> GetLeaveTypesAsync(); 
+        Task<bool> CreateLeaveTypeAsync(LeaveTypeDto dto);
+        Task<bool> UpdateLeaveTypeAsync(LeaveTypeDto dto);
+        Task<bool> DeleteLeaveTypeAsync(int id);
+    }
+}
