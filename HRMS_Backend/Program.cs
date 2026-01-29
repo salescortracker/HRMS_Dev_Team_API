dc58d07fd29811b7e6d88bdd57dc86dec0d7dcec
Bhavyasree001 <bhavyasree@cortracker360.com>
2026-01-29 11:43:24 +0530

    Timesheet-Api

--- HRMS_Backend/Program.cs
+++ HRMS_Backend/Program.cs
@@ -33,6 +33,8 @@ builder.Services.AddScoped<IRoleMasterService, RoleMasterService>();
 builder.Services.AddScoped<IMenuRoleService, MenuRoleService>();
 builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 builder.Services.AddScoped<IDesignationService, DesignationService>();
+builder.Services.AddScoped<ITimesheetService, TimesheetService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
