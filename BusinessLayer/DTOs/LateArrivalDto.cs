b4e825fd40f0d601264fe7040f8968f753b72a18
KishoreSetti <kishore@cortracker360.com>
2026-02-03 15:14:34 +0530

    late_arrivals_API
    late_arrivals_API

---
+++ BusinessLayer/DTOs/LateArrivalDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTOs
+{
+    
+        public class LateArrivalDto
+        {
+        public string EmployeeName { get; set; }
+        public string EmployeeCode { get; set; }
+        public DateOnly AttendanceDate { get; set; }
+        public TimeOnly ShiftStartTime { get; set; }
+        public TimeOnly AllowedClockInTime { get; set; }
+        public TimeOnly? ClockInTime { get; set; }
+        public int LateByMinutes { get; set; }
+        public string Status { get; set; }
+    }
+    
+}
---
+++ BusinessLayer/Interfaces/ILateArrivalService.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+   
+    public interface ILateArrivalService
+    {
+        Task<List<LateArrivalDto>> GetLateArrivalsAsync(
+        int companyId,
+        int regionId,
+        DateOnly fromDate,
+        DateOnly toDate,
+        string? employeeCode
+    );
+    }
+
+}
